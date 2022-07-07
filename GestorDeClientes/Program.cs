6483492e1ec3422922e062f3e08e1c29f41ebc09
Higor Jugler <higorjugler@hotmail.com>
2022-07-07 15:11:33 -0300

    Desenvolvido menu do app

--- GestorDeClientes/Program.cs
+++ GestorDeClientes/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace GestorDeClientes
+{
+    enum Menu { Adicionar = 1, Listar = 2, Editar = 3, Sair = 4 }
+    class Program
+    {
+        static void Main(string[] args)
+        {                
+
+            bool sairApp = false;
+            while(!sairApp)
+            {
+                Console.WriteLine("Bem vindo ao app Gestor de Clientes");
+                Console.WriteLine("Selecione a opção desejada:\n");
+                Console.WriteLine("1 - Adicionar novo contato\n" +
+                    "2 - Listar contatos adicionados\n" +
+                    "3 - Editar contatos\n" +
+                    "4 - Sair do Gestor de Clientes\n");
+
+                Menu menu = new Menu();
+                menu = (Menu)int.Parse(Console.ReadLine());
+
+                switch (menu)
+                {                    
+                    case Menu.Adicionar:
+                        break;
+                    case Menu.Listar:
+                        break;
+                    case Menu.Editar:
+                        break;
+                    case Menu.Sair:
+                        sairApp = true;
+                        break;
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
